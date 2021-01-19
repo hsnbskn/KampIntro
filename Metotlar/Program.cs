8be19c2ed7ec4b4de0760a82c0c7f8e66a5c6869
hsnbskn <bms.kurtdemir@outlook.com>
2021-01-19 23:06:30 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakir karpuzu";
+
+            //Tıpkı string array'i oluşturduğum gibi class kullanarakta urun array'i oluşturabilirm.
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli (var yazsakta olur ama güvenli değil)
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------------");
+
+            }
+
+            Console.WriteLine("-----------------Metotlar-----------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            
+
+
+
+
+        }
+    }
+}
+
+
+
